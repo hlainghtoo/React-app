b99ded0cca3419552fbbbcedb7dc70383c1e658b
hlainghtoo <hlainghtooedu@gmail.com>
2024-09-29 11:44:29 +0700

    react-app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import
+import React from 'react'
+// import ResponsiveAppBar from './Appbar'
+import FetchData from './FetchData'
+
+// function
+const App = () => {
+  return(
+    <div>
+      {/* <ResponsiveAppBar /> */}
+      <FetchData />
+    </div>
+  )
+}
+
+// export default App
+export default App
